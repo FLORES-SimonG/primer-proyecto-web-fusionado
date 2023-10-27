e7ed6a0ef4a1a67c45bd6280c5cdb6efab8621f9
Simon G. Flores <simonflores_10@hotmail.com>
2023-10-27 09:44:41 +0200

    Agregué el código de clase12.html a index.html y styles.css. En main.js realicé también modificaciones de nombres como también en CSS. También pequeños cambios en mobile-menu y entre otros.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');/*modifiqué el nombre aca de aside paso a shoppingcartCotainer*/
 const cardsContainer = document.querySelector('.cards-container');
 
 
@@ -14,19 +14,19 @@ menuCarritoIcon.addEventListener('click',toggleCarritoAside);
 
 
 function toggleDesktopMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if (!isAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');   
 }
 function toggleMobileMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if (!isAsideClosed){
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     mobileMenu.classList.toggle('inactive');   
@@ -37,7 +37,7 @@ function toggleCarritoAside(){
     if (!ismobileMenuClosed){
         mobileMenu.classList.add('inactive');
     }
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 const productList=[];
